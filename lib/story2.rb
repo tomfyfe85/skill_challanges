d7a02ebf4c9ad6e1e290804e36abb80e199bdb48
Tom Fyfe <tomfyfe@MASTUDENT-Tom-Fyfe-W7JGKXKG9H.local>
2022-10-13 10:45:18 +0100

    part2. story2-updated for last Char == '.'/'?'/!

--- lib/story2.rb
+++ lib/story2.rb
@@ -1,5 +1,6 @@
 def story2(str)
-  if str[0] == str[0].upcase && str[-1] == '.'
+  if str[0] == str[0].upcase && str[-1] == '.' || str[-1] == '!' || str[-1] == '?'
     return str
   end
 end 
+puts story2('Hello!')
